2a37ac6d6ba6fe7a60baff0cd4611f1b97dfc6c9
villainbot1 <148652669+villainbot1@users.noreply.github.com>
2023-11-09 21:20:38 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -172,7 +172,7 @@ async def start_comm(client, message: Message, _):
                             text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/DevilsHeavenMF"
+                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/DomainXSupport"
                         ),
                     ],
                 ]
